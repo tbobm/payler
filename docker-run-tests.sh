e64ee50c2f2878b01fef93488ac6e5f36a0d3be1
Theo Massard <theo@memento-solution.com>
2020-12-07 22:58:55 +0100

    Create docker test network
    Variabilize test image building

--- docker-run-tests.sh
+++ docker-run-tests.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
-# docker build -t payler:base .
-# docker build -t payler:tests -f Dockerfile.test .
+if [ "$1" == "--build" ]
+then
+    docker build -t payler:base .
+    docker build -t payler:tests -f Dockerfile.test .
+fi
 
 # ./run-background-service.sh
 
-docker run -i --rm \
+docker run -it --rm \
     -e MONGODB_URL='mongodb://payler:secret@payler_storage/payloads?authSource=admin' \
+    --network payler_local \
     --name payler_tests \
     payler:tests
