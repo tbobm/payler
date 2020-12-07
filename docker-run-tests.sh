415ef1e7afeb5b96052069004e95e1da520416e5
Theo Massard <theo@memento-solution.com>
2020-12-07 23:01:31 +0100

    Improve development environment configuration
    Use custom local network
    Implement RabbitMQ service
    
    References #11

--- docker-run-tests.sh
+++ docker-run-tests.sh
@@ -9,6 +9,7 @@ fi
 
 docker run -it --rm \
     -e MONGODB_URL='mongodb://payler:secret@payler_storage/payloads?authSource=admin' \
+    -e BROKER_URL='amqp://payler:secret@payler_storage/payler' \
     --network payler_local \
     --name payler_tests \
     payler:tests
